d79714dd23178bb736cf6beee4b124483bc1e787
Konitsuga <25f2008715@ds.study.iitm.ac.in>
2026-01-23 00:09:25 +0530

    Implement factorial function in day3.py
    Added a factorial function using recursion.

--- day3.py
+++ day3.py
@@ -3,3 +3,8 @@ def natural_numbers(n):
         return 1
     return n, natural_numbers(n-1)
 print(natural_numbers(67))
+
+def factorial(n): # recursion for factorial
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial(n-1)
